21fbc62ca8a9308f1434739528ed78b1b24f0e8e
janec123 <janec@stud.uniza.sk>
2025-08-01 19:04:41 +0200

    YieldsFeature

---
+++ Yeild.cs
@@ -0,0 +1,44 @@
+﻿using C_Advanced;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Advanced
+{
+    class Yeild
+    {
+       
+
+        public Yeild()
+        {
+          
+        }
+        public IEnumerable<string> GetYeildList()
+        {
+
+            for (int i = 0; i < 1_000_000; i++)
+            {
+               yield return $"Item {i}";
+            }
+        }
+    }
+}
+
+//public static void Main(string[] args)
+//{
+//    Yeild yeild = new Yeild();
+
+
+
+
+//    foreach (var item in yeild.GetYeildList())
+//    {
+//        Console.WriteLine($"{item}");
+//    }
+
+
+
+//}
+
