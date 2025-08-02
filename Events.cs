d8b2a50b7374f9291b9a913941eb9c15931b2886
janec123 <janec@stud.uniza.sk>
2025-08-02 15:11:25 +0200

    generic

---
+++ Events.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using static YoutubeChannel;
+
+class YoutubeChannel
+{
+
+    public class VideoEventArgs : EventArgs
+    {
+        public string VideoTitle { get; set; }
+        public string VideoDescription { get; set; }
+      
+    }
+
+    public event EventHandler<VideoEventArgs>? VideoUploaded;
+
+    public void UploadVideo()
+    {
+
+
+        VideoUploaded?.Invoke(this, new VideoEventArgs()
+        {
+            VideoTitle = "C# Advanced Programming",
+            VideoDescription = "Learn advanced C# concepts in this video."
+        });
+    }
+
+    public void NumberOfSubscribers() 
+    { 
+        if(VideoUploaded != null)
+        {
+            Console.WriteLine("There are subscribers to this channel.");
+        }
+        else
+        {
+            Console.WriteLine("There are no subscribers to this channel.");
+        }
+    }
+
+}
+
+
+
+
+class Person
+{
+    public string Name;
+
+
+    public Person(YoutubeChannel channel, string name)
+    {
+     
+            channel.VideoUploaded += Channel_VideoUploaded;
+            Name = name;
+    }
+
+    private void Channel_VideoUploaded(object? sender, VideoEventArgs e)
+    {
+        Console.WriteLine($"Person {Name} has been notiffied with video: {e.VideoTitle}");
+
+    }
+
+    public void Unsubscribe(YoutubeChannel channel)
+    {
+        channel.VideoUploaded -= Channel_VideoUploaded;
+    
+    }
+}
+
+
+
+//public static void Main(string[] args)
+//{
+//    YoutubeChannel channel = new YoutubeChannel();
+//    Person person1 = new Person(channel, "Alice");
+//    channel.NumberOfSubscribers();
+//    channel.UploadVideo();
+//    person1.Unsubscribe(channel);
+//    channel.NumberOfSubscribers();
+//}
---
+++ Generic.cs
@@ -0,0 +1,67 @@
+﻿using C_Advanced;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Advanced
+{
+    class InvenotryItem
+    {
+        public string Name { get; set; }
+
+
+
+        public virtual void itemUse()
+        {
+            Console.WriteLine("Item was used.");
+        }
+    }
+
+    class Sword : InvenotryItem
+    {
+        public int Damage { get; set; }
+        public Sword(string name, int damage)
+        {
+            Name = name;
+            Damage = damage;
+        }
+        public void Swing()
+        {
+            Console.WriteLine($"{Name} swings with {Damage} damage.");
+        }
+
+         public override void itemUse()
+        {
+            Swing();
+        }
+    }
+
+
+    class Inventory<T> where T : InvenotryItem
+    {
+        public T Item { get; set; }
+
+        public Inventory(T item)
+        {
+            Item = item;
+        }
+
+        public void UseItem()
+        {
+            Item.itemUse();
+        }
+    }
+}
+
+
+//public static void Main(string[] args)
+//{
+//    InvenotryItem item = new InvenotryItem { Name = "Health Potion" };
+//    Sword sword = new Sword("Excalibur", 50);
+//    Inventory<Sword> inventoryItem = new Inventory<Sword>(sword);
+//    inventoryItem.UseItem();
+
+
+//}
