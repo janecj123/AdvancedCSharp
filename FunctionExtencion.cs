a432cdaa40e408c7224ca3b7d80e3bc4484651e8
janec123 <janec@stud.uniza.sk>
2025-08-03 15:22:07 +0200

    Reflection+FunctionExtencion

---
+++ FunctionExtencion.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// function extencion allows you to add new methods to existing types without modifying the original type or creating a new derived type.
+// it also can add methods to bulid in types like string, int, etc.
+//it basicly means that you can add methods to every existing class if you follow the syntax
+
+namespace FunctionExtencion
+{
+    //public static void Main(string[] args)
+    //{
+    //    Player player = new Player();
+    //    player.Name = "Hero"; // setting the player's name
+    //    player.Start();
+    //    player.MoveRight(5); // using the extension method to move the player to the right 5 steps
+
+
+    //    int number = 10;
+    //    number.AddFive();
+    //    Console.WriteLine($"Number after adding five: {number}"); // using the extension method to add five to the number
+    //}
+
+
+
+
+
+public class Player
+{
+    public string Name { get; set; }
+    public void Start()
+    {
+        Console.WriteLine("Player started!");
+    }
+}
+
+
+public static class FunctionExtention
+{
+
+        //function need to be static and first parameter need to have this keyword and name of the class/type you want to extend
+        //you can allso access Player properties and methods inside the extension method
+        public static void MoveRight(this Player player, int distance)
+    {
+        Console.WriteLine($"Player moved to the right {distance} steps !");
+        Console.WriteLine($"Player name is: {player.Name}");
+    }
+
+    public static int AddFive(this int number)
+    {
+        return number + 5;
+    }
+}
+
+   
+
+}
---
+++ Reflection.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+// reflection is used to check if a class has a specific method, property or neasted another class at runtime
+namespace Reflection
+
+{
+    public class Player
+    {
+
+        public class Inventory
+        {
+
+            private void AddItem(string item)
+            {
+                Console.WriteLine($"Adding {item} to inventory.");
+            }
+        }
+
+
+
+
+
+
+
+
+    //public static void Main(string[] args)
+    //{
+    //    Player.Inventory inventory = new Player.Inventory();
+
+
+
+    //    //typeof(Player).GetNestedType("Inventory").GetMethod("AddItem").Invoke(inventory, new object[] { "Sword" });
+
+    //    //can access private method using reflection but need to add | System.Reflection.BindingFlags.Instance in get method parameters
+    //    typeof(Player).GetNestedType("Inventory").GetMethod("AddItem", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance).Invoke(inventory, new object[] { "Sword" });
+
+    //    // this is another way to access private method using reflection 
+    //    //it is doing the as same as the above code but in a more verbose way
+    //    Type inventoryType = typeof(Player).GetNestedType("Inventory");
+    //    System.Reflection.MethodInfo methodInfo = inventoryType.GetMethod("AddItem", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+    //    methodInfo.Invoke(inventory, new object[] { "Shield" });
+
+
+    //    // if Player.Inventory is a class that has a parameterless constructor, you can create an instance of it using Activator.CreateInstance
+    //    if (inventoryType != null)
+    //    {
+    //        object inventoryObject = Activator.CreateInstance(inventoryType);
+
+    //    }
+
+    //}
+
+
+}
+
+
+
+}
