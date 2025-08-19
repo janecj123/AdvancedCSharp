110811d3303b7df59422e6ccc8f04f62b8a94e06
janec123 <janec@stud.uniza.sk>
2025-08-19 18:08:17 +0200

    Records

---
+++ Record.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Advanced
+{
+
+    //Records automaticly creat methods Equal and GetHeshCode where its comparing fields values not references!!!!
+
+
+    //class Program
+    //{
+
+
+    //    public static void Main(string[] args)
+    //    {
+    //        var items = new Items("Hammer", "Axe");
+    //        var items2 = new Items("Hammer", "Axe");
+
+
+    //        Console.WriteLine(items == items2);
+
+    //        var itemsStruct = new ItemsStruct("Hammer", "Axe");
+    //        var itemsStruct2 = new ItemsStruct("Hammer", "Axe");
+
+    //        Console.WriteLine(itemsStruct == itemsStruct2);
+
+    //    }
+
+
+
+
+
+
+    //}
+    //public record Items(string First, string Second);
+    //public record struct ItemsStruct(string First, string Second);
+}
